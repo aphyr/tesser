f6567e4b9f6549f5420ed0bf2ef63241d05ad47c
Aphyr <aphyr@aphyr.com>
2016-09-07 20:30:03 -0500

    docs

--- all/project.clj
+++ all/project.clj
@@ -1,21 +1,20 @@
-(defproject tesser.all "0.1.0-SNAPSHOT"
+(defproject tesser.all "1.0.2"
   :description "Not a real library; just a placeholder for unifying all the docs."
   :url "http://github.com/aphyr/tesser"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos/"}
-  :dependencies [[tesser.core   "0.1.0-SNAPSHOT"]
-                 [tesser.math   "0.1.0-SNAPSHOT"]
-                 [tesser.hadoop "0.1.0-SNAPSHOT"
+  :dependencies [[tesser.core   "1.0.2"]
+                 [tesser.math   "1.0.2"]
+                 [tesser.hadoop "1.0.2"
                   :exclusions [org.codehaus.jackson/jackson-mapper-asl
                                org.codehaus.jackson/jackson-core-asl]]]
   :codox {:sources ["../core/src"
                     "../math/src"
                     "../hadoop/src"]
-          ; Huh, codox strips off a master.
-          :src-dir-uri "http://github.com/aphyr/tesser/blob/master/master/"
-          :src-linenum-anchor-prefix "L"
-          :defaults {:doc/format :markdown}}
+          :output-path "doc/"
+          :source-uri "http://github.com/aphyr/tesser/blob/{version}/{filepath}#L{line}"
+          :metadata {:doc/format :markdown}}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}
              ; Parkour won't compile without hadoop
              :provided
