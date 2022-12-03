7b691952202bbb181e1036f9799f7f0b3a177ca2
Aphyr <aphyr@aphyr.com>
2022-12-02 22:55:34 -0500

    tesser.math 1.0.6-SNAPSHOT

--- math/project.clj
+++ math/project.clj
@@ -1,10 +1,10 @@
-(defproject tesser.math "1.0.5"
+(defproject tesser.math "1.0.6-SNAPSHOT"
   :description "Concurrent folds for statistical analysis"
   :url "http://github.com/aphyr/tesser"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[tesser.core "1.0.5"]
+  :dependencies [[tesser.core "1.0.6-SNAPSHOT"]
 ;                 [com.tdunning/t-digest "3.0"]
                  [com.clearspring.analytics/stream "2.9.8"]
                  ; 2.1.10 changed quantiles; see https://github.com/HdrHistogram/HdrHistogram/issues/194
