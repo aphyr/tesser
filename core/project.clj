c4563e2a33ca66bd724a077db62d75d2c0e29ee8
Kyle Kingsbury <aphyr@aphyr.com>
2026-07-17 12:46:21 -0500

    tesser.math deps\nmath.combinatorics 0.3.2, numeric-tower 0.1.1, clojure 1.12.5, test.check 1.1.3

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject tesser.core "1.0.9-SNAPSHOT"
+(defproject tesser.core "1.0.9"
   :description "Composable concurrent folds for Clojure."
   :url "http://github.com/aphyr/tesser"
   :license {:name "Eclipse Public License"
--- math/project.clj
+++ math/project.clj
@@ -1,17 +1,17 @@
-(defproject tesser.math "1.0.9-SNAPSHOT"
+(defproject tesser.math "1.0.9"
   :description "Concurrent folds for statistical analysis"
   :url "http://github.com/aphyr/tesser"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[tesser.core "1.0.8"]
+  :dependencies [[tesser.core "1.0.9"]
 ;                 [com.tdunning/t-digest "3.0"]
                  [com.clearspring.analytics/stream "2.9.8"]
                  ; 2.1.10 changed quantiles; see https://github.com/HdrHistogram/HdrHistogram/issues/194.
                  [org.hdrhistogram/HdrHistogram "2.1.9"]
-                 [org.clojure/math.combinatorics "0.3.0"]
-                 [org.clojure/math.numeric-tower "0.1.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.3"]
+                 [org.clojure/math.combinatorics "0.3.2"]
+                 [org.clojure/math.numeric-tower "0.1.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.5"]
                                   [metametadata/multiset "0.1.1"]
                                   [criterium "0.4.6"]
-                                  [org.clojure/test.check "1.1.1"]]}})
+                                  [org.clojure/test.check "1.1.3"]]}})
